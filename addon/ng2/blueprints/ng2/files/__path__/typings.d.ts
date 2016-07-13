26aeafb8f591ac5a0c2678b2f8844ebb62153757
Filipe Silva <filipematossilva@gmail.com>
2016-07-13 01:57:33 +0100

    chore: update to ts2.0 and @types

--- addon/ng2/blueprints/ng2/files/__path__/typings.d.ts
+++ addon/ng2/blueprints/ng2/files/__path__/typings.d.ts
@@ -3,7 +3,6 @@
 // https://github.com/typings/typings
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
 
-/// <reference path="<%= refToTypings %>/typings/index.d.ts" />
 /// <reference path="../node_modules/typescript/lib/lib.es2015.core.d.ts" />
 /// <reference path="../node_modules/typescript/lib/lib.es2015.collection.d.ts" />
 /// <reference path="../node_modules/typescript/lib/lib.es2015.promise.d.ts" />
